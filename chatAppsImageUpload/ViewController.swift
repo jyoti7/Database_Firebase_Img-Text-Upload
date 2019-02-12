db4636c99e9dee3f8ab0b01402d8400ef32eaa9c
jyoti7 <jotey77@gmail.com>
2019-02-12 16:00:22 +0600

    Successfully image upload

--- chatAppsImageUpload/ViewController.swift
+++ chatAppsImageUpload/ViewController.swift
@@ -42,15 +42,23 @@ class ViewController: UIViewController {
     
     
     func saveFIRData(){
-        let  dict = ["name" : "Jyoti", "text": txtField.text!]
+        self.UPLOADimg(_image: self.uploadImg.image!){ url in
+            
+            self.saveImage(name: self.txtField.text!, profileURL: url!){ success in
+                if success != nil {
+                    print("Yes")
+                }
+                
+            }
+            
+        }
         
-        self.ref.child("chatApps").childByAutoId().setValue(dict)
     }
 }
 
 extension ViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
     
-    
+    //for open gallary
     func setupImagePicker(){
         if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum)
         {
@@ -62,9 +70,44 @@ extension ViewController: UIImagePickerControllerDelegate,UINavigationController
         }
     }
     
+    // delegate mathod for pick image from gallary
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         uploadImg.image = image
         self.dismiss(animated: true, completion: nil)
     }
+}
+
+extension ViewController{
+    // For Upload image
+    func UPLOADimg (_image:UIImage, completion:@escaping ((_ url:URL? ) -> ())){
+    let storageRef = Storage.storage().reference().child("my_Image.png")
+        let imgData = uploadImg.image?.pngData()
+        let metaData = StorageMetadata()
+        //metaData.contentType = "image/png"
+        storageRef.putData(imgData!, metadata: metaData) { (metadata, error) in
+            if error == nil {
+                print("SUCCESS")
+                
+                storageRef.downloadURL(completion: { (url, error) in
+                    completion (url)
+                })
+            }else{
+                print("Error in save image")
+                completion(nil)
+            }
+        }
+    }
+    
+    
+            /*let  dict = ["name" : "Jyoti", "text": txtField.text!]
+            self.ref.child("chatApps").childByAutoId().setValue(dict)*/
+    
+    //For save image
+    func saveImage(name: String, profileURL:URL, completion:@escaping((_ url:URL? ) -> ())){
+        let  dict = ["name" : "Jyoti", "text": txtField.text!, "profileUrl":profileURL.absoluteString] as [String: Any]
+        self.ref.child("chatApps").childByAutoId().setValue(dict)
+        
+    }
+    
 }
